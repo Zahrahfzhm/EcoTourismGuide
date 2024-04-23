5efbd72733f5d0a5ce27f253eb10ce3e699678a1
aviesena <aviesenarabbani@gmail.com>
2024-04-23 17:04:44 +0700

    Merge from aviesena_rabbani

--- ecotourismguide_next/src/components/Container/DashboardWisatawan/index.js
+++ ecotourismguide_next/src/components/Container/DashboardWisatawan/index.js
@@ -1,6 +1,6 @@
 export default function DashboardWisatawanContainer() {
     return(
-        <section className="bg-[#B9F4E0] w-dvh h-[580px]">
+        <section className="bg-[#B9F4E0] w-dvh h-[1000px] mt-2">
         </section>
     )
 }
--- ecotourismguide_next/src/components/Navbar/header.js
+++ ecotourismguide_next/src/components/Navbar/header.js
@@ -1,12 +1,13 @@
+import Image from 'next/image';
 
 export default function nav() {
     return(
         <nav>
             <div className="flex items-center justify-between mt-0 text-lg bg-white h-[70px]">
-                <div className="ml-4 p-2"> 
-                    <h1 className="text-xl font-bold">Eco Tourism Guide</h1>
+                <div className="ml-6 mt-1 p-2"> 
+                    <Image src="/logo_landscape.png" alt="Logo" width={180} height={120} /> 
                 </div> 
-                <div className="space-x-20 mr-4 p-2">
+                <div className="space-x-20 mr-6 p-2">
                     <button className="hover:bg-[#1EC28B] hover:shadow-md hover:text-white text-black px-4 py-2 rounded-lg transition-colors duration-300 ease-in-out">
                         Contact Us
                     </button>
