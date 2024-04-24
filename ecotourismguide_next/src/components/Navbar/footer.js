9a32ba8fcf9f63abe557e0feea2024249581aada
aviesena <aviesenarabbani@gmail.com>
2024-04-24 19:18:11 +0700

    bang balmon

--- ecotourismguide_next/src/components/Navbar/footer.js
+++ ecotourismguide_next/src/components/Navbar/footer.js
@@ -1,24 +1,38 @@
-
-import Link from "next/link"
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <footer className="bg-[#2B2945] py-4 flex justify-left  fixed bottom-0 h-[200px] w-full">
-            <div className="ml-4 p-2">
-                <div className="flex justify-between w-[220px]">
-                    <Link href='/' className="text-white text-md hover:transform hover:scale-110 transition-transform duration-300 ">
-                        Developer 
-                    </Link>
-                    <Link href='/' className="text-white text-md hover:transform hover:scale-110 transition-transform duration-300">
-                        About Us
-                    </Link>
+        <footer className="bg-[#2B2945] text-white py-8 px-4 fixed bottom-0 w-full">
+            <div className="flex justify-center mb-8">
+                <div className="w-1/4">
+                    <img src="/logo_landscape_white.png" alt="ECO-TOURISM GUIDE" className="w-auto h-20 mb-4" />
+                    <p className="mt-4 mr-6">Menyediakan informasi tentang destinasi pariwisata yang ramah lingkungan dan berkualitas, serta kegiatan wisata yang mendukung pelestarian alam dan keberlanjutan ekonomi lokal.</p>
                 </div>
-                <div className="flex mt-6">
-                    <Link href='/' className="text-white text-md hover:transform hover:scale-110 transition-transform duration-300">
-                        Sosmed
-                    </Link>
+                <div className="w-1/8 mr-20">
+                    <h2 className="text-lg font-semibold mb-1 h-8">Company</h2>
+                    <ul>
+                        <li><a href="#" className="hover:text-gray-300">About us</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Contact us</a></li>
+                        <li><a href="#" className="hover:text-gray-300">FAQs</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Search</a></li>
+                    </ul>
+                </div>
+                <div className="w-1/8 mr-20">
+                    <h2 className="text-lg font-semibold mb-1 h-8">Support</h2>
+                    <img src="/kamenkraft.svg" alt="Kemenkrat" className="w-auto h-[120px] mb-1" />
+                </div>
+                <div className="w-1/8 mr-15">
+                    <h2 className="text-lg font-semibold mb-1 h-8">Social Media</h2>
+                    <div className="flex space-x-4">
+                        <FaFacebookF className="text-2xl hover:text-gray-300 cursor-pointer" />
+                        <FaInstagram className="text-2xl hover:text-gray-300 cursor-pointer" />
+                        <FaTwitter className="text-2xl hover:text-gray-300 cursor-pointer" />
+                        <FaLinkedinIn className="text-2xl hover:text-gray-300 cursor-pointer" />
+                    </div>
                 </div>
             </div>
+            <hr className="border-gray-600 my-8" />
+            <p className="text-center">&copy; 2024 EcoTourismGuide. All rights reserved</p>
         </footer>
-    )
+    );
 }
