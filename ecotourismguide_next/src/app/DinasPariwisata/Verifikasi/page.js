0acf01769cc3e1b2c4ba30924ca3a4a1681d6e14
naufaleka03 <naufaleka03@gmail.com>
2024-04-29 16:56:54 +0700

    Commit Pengerjaan Rutin

---
+++ ecotourismguide_next/src/app/DinasPariwisata/Verifikasi/page.js
@@ -0,0 +1,9 @@
+import PageVerifikasi1 from "@/components/Container/VerifikasiAdmin";
+
+export default function Verifikasi(){
+    return( 
+       <div className="flex space-x-6">
+            <p></p>
+       </div> 
+    );
+}
---
+++ ecotourismguide_next/src/components/Content/VerifikasiRegistrasi/monitor.js
@@ -0,0 +1,26 @@
+export default function MonitorVerifikasi(){
+    return(
+        <div className="max-w-sm mx-auto my-6 px-6 py-6 rounded-lg shadow-md">
+            <div>
+                <p className="text-xl font-semibold">Menunggu Konfirmasi</p>
+                <p className="mt-16 text-base font-normal">Total dokumen:</p>
+                <p className="text-lg font-semibold">200</p>
+            </div>
+            <hr className="my-5 w-full"></hr>
+            <div>
+                <p className="text-xl font-semibold">Diterima</p>
+                <p className="mt-16 text-base font-normal">Total dokumen:</p>
+                <p className="text-lg font-semibold">200</p>
+            </div>
+            <hr className="my-5 w-full"></hr>
+            <div>
+                <p className="text-xl font-semibold">Ditolak / Dikembalikan</p>
+                <p className="mt-16 text-base font-normal">Total dokumen:</p>
+                <p className="text-lg font-semibold">200</p>
+            </div>
+        </div>
+    );
+}
+
+
+
