ab8fc798768aad153ad91a86121d794c26ddf909
naufaleka03 <naufaleka03@gmail.com>
2024-05-31 21:25:58 +0700

    Minor changes

--- ecotourismguide_next/src/app/page.js
+++ ecotourismguide_next/src/app/page.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import Header from "../components/Navbar/header";
-import Footer from "../components/Navbar/footer";
-import DashboardWisatawanContainer from "../components/Container/DashboardWisatawan";
-=======
 import WisatawanContainer from "@/components/Container/DashboardWisatawan";
 import Header from "@/components/Navbar/header";
 import Footer from "@/components/Navbar/footer";
 import DashboardWisatawanContent from "@/components/Content/DashboardWisatawan/index";
->>>>>>> 4f7d51d9bb3c5b2ad15125529162c72943d1825d
 
 export default function Home() {
   return (
--- ecotourismguide_next/src/components/Content/VerifikasiRegistrasi/table.js
+++ ecotourismguide_next/src/components/Content/VerifikasiRegistrasi/table.js
@@ -43,37 +43,68 @@ export default function TabelVerifikasi(){
                     <table className="w-full border border-collapse border-slate-300 table-auto">
                         <thead className="bg-[#1EC28B] text-sm font-bold text-white">
                             <tr>
-                                <th className="px-4 py-2 text-left font-semibold">No</th>
-                                <th className="px-4 py-2 text-left font-semibold">Column Name</th>
-                                <th className="px-4 py-2 text-left font-semibold">Column Name</th>
-                                <th className="px-4 py-2 text-left font-semibold">Column Name</th>
-                                <th className="px-4 py-2 text-left font-semibold">Column Name</th>
-                                <th className="px-4 py-2"></th>
+                                <th className="px-4 py-2 text-center font-semibold">No</th>
+                                <th className="px-4 py-2 text-center font-semibold">Foto Destinasi</th>
+                                <th className="px-4 py-2 text-center font-semibold">Nama Destinasi</th>
+                                <th className="px-4 py-2 text-center font-semibold">Tanggal Kunjungan</th>
+                                <th className="px-4 py-2 text-center font-semibold">Status</th>
+                                <th className="px-4 py-2 text-center"></th>
                             </tr>
                         </thead>
                         <tbody className="text-sm font-normal">
                             <tr>
-                                <td className="px-4 py-2">1</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">
+                                <td className="px-4 py-2 text-center">1</td>
+                                <td className="px-4 py-2 text-center">
+                                    <div className="flex justify-center">
+                                        <div className="w-20 h-20 rounded-lg shadow-md overflow-hidden">
+                                            <img src="https://picsum.photos/250/250?random=1" alt="Foto Destinasi 1" className="w-full h-full object-cover"/>
+                                        </div>   
+                                    </div>
+                                </td>
+                                <td className="px-4 py-2 text-center">Destinasi A</td>
+                                <td className="px-4 py-2 text-center">01-01-2022</td>
+                                <td className="px-4 py-2 text-center">Menunggu konfirmasi</td>
+                                <td className="px-4 py-2 text-center">
                                     <button className="bg-[#FF8345] hover:bg-[#c17a3a] text-white font-medium py-2 px-2 rounded">
-                                        Button
+                                        Periksa
                                     </button>
                                 </td>
                             </tr>
 
                             <tr className="bg-gray-100">
-                                <td className="px-4 py-2">2</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">Content</td>
-                                <td className="px-4 py-2">
+                                <td className="px-4 py-2 text-center">2</td>
+                                <td className="px-4 py-2 text-center">
+                                    <div className="flex justify-center">
+                                        <div className="w-20 h-20 rounded-lg shadow-md overflow-hidden">
+                                            <img src="https://picsum.photos/200/200?random=2" alt="Foto Destinasi 2" className="w-full h-full object-cover"/>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="px-4 py-2 text-center">Destinasi B</td>
+                                <td className="px-4 py-2 text-center">15-02-2022</td>
+                                <td className="px-4 py-2 text-center">Diterima</td>
+                                <td className="px-4 py-2 text-center">
                                     <button className="bg-[#FF8345] hover:bg-[#c17a3a] text-white font-medium py-2 px-2 rounded">
-                                        Button
+                                        Periksa
+                                    </button>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td className="px-4 py-2 text-center">3</td>
+                                <td className="px-4 py-2 text-center">
+                                    <div className="flex justify-center">
+                                        <div className="w-20 h-20 rounded-lg shadow-md overflow-hidden">
+                                            <img src="https://picsum.photos/200/200?random=3" alt="Foto Destinasi 3" className="w-full h-full object-cover"/>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="px-4 py-2 text-center">Destinasi C</td>
+                                <td className="px-4 py-2 text-center">20-03-2022</td>
+                                <td className="px-4 py-2 text-center">Ditolak / dikembalikan</td>
+                                <td className="px-4 py-2 text-center">
+                                    <button className="bg-[#FF8345] hover:bg-[#c17a3a] text-white font-medium py-2 px-2 rounded">
+                                        Periksa
                                     </button>
                                 </td>
                             </tr>
